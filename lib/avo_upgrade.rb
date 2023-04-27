9abe7222e284db2662285212f79f3a014250e3dc
Adrian <adrian@adrianthedev.com>
2023-04-27 15:24:29 +0300

    initial commit

---
+++ lib/avo_upgrade.rb
@@ -0,0 +1,6 @@
+require "avo_upgrade/version"
+require "avo_upgrade/railtie"
+
+module AvoUpgrade
+  # Your code goes here...
+end
---
+++ lib/avo_upgrade/railtie.rb
@@ -0,0 +1,4 @@
+module AvoUpgrade
+  class Railtie < ::Rails::Railtie
+  end
+end
