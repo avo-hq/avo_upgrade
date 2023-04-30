6712e3ae2c82c37437920c121469727e60c9816b
Paul Bob <paul.ionut.bob@gmail.com>
2023-04-30 16:07:55 +0100

    readability is cool

--- lib/avo_upgrade/upgrade29to30.rb
+++ lib/avo_upgrade/upgrade29to30.rb
@@ -1,15 +1,25 @@
 module AvoUpgrade
   class Upgrade29to30 < AvoUpgrade::UpgradeTool
     def run
-      # replace_in_filename "_resource", "", path: resources_path
+      replace_in_filename "_resource", "", path: resources_path
 
-      replace_avo_global_text (class_names_for(:resources).map do |class_name|
+      old_text_new_text_hash = class_names_for(:resources).map do |class_name|
         [class_name, "Avo::Resources::#{class_name.sub(/Resource$/, '')}"]
-      end + [:actions, :filters, :resource_tools].map do |component|
-        class_names_for(component).map do |class_name|
-          [class_name, "Avo::#{component.to_s.camelize.pluralize}::#{class_name}"]
-        end
-      end.flatten(1)).to_h
+      end.to_h
+
+      [:actions, :filters, :resource_tools].each do |component|
+        old_text_new_text_hash.merge! change_class_name_hash_for(component)
+      end
+
+      replace_avo_global_text old_text_new_text_hash
+    end
+
+    private
+
+    def change_class_name_hash_for(component)
+      pluralized_component = component.to_s.camelize.pluralize
+
+      class_names_for(component).map { |class_name| [class_name, "Avo::#{pluralized_component}::#{class_name}"] }.to_h
     end
   end
 end
